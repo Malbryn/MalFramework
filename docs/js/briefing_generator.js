c74f6a6cc96caacf9d834f6c9e084be9a9dcdb14
Dániel Boros <daniel.boros.17@gmail.com>
2021-05-22 19:25:52 +0200

    Fix array indexing and remove bullet points

--- docs/js/briefing_generator.js
+++ docs/js/briefing_generator.js
@@ -32,8 +32,10 @@ function generateBriefing(fields) {
         element['value'] = convertLineBreaks(element['value']);
     });
 
+    console.log(fields[27]['value']);
+
     // Select side colour
-    let sideColour = fields[29]['value'] === "BLUFOR" ? "#21749c" : "#9c2d21";
+    let sideColour = fields[27]['value'] === "BLUFOR" ? "#21749c" : "#9c2d21";
 
     // Output
     let template = `// This briefing file was generated with the Briefing Builder tool
@@ -56,16 +58,16 @@ NEWTAB("II. Situation")
 <br/>
 <br/><font color='${sideColour}' size='14' face='PuristaSemiBold'>Weather</font>
 <br/><font size='12' face='PuristaMedium'>Current:</font>
-<br/> - ${fields[4]['value']}
+<br/>${fields[4]['value']}
 <br/>
 <br/><font size='12' face='PuristaMedium'>Forecast:</font>
-<br/> - ${fields[5]['value']}
+<br/>${fields[5]['value']}
 <br/>
 <br/><font size='12' face='PuristaMedium'>Visibility:</font>
-<br/> - ${fields[6]['value']}
+<br/>${fields[6]['value']}
 <br/>
 <br/><font color='${sideColour}' size='14' face='PuristaSemiBold'>Civilian presence</font>
-<br/> - ${fields[7]['value']}
+<br/>${fields[7]['value']}
 <br/>
 <br/>
 <br/><font color='${sideColour}' size='18' face='PuristaBold'>ENEMY FORCES</font>
@@ -74,81 +76,76 @@ NEWTAB("II. Situation")
 <br/>
 <br/><font color='${sideColour}' size='14' face='PuristaSemiBold'>Capabilities:</font>
 <br/><font size='12' face='PuristaMedium'>Armour:</font>
-<br/> - ${fields[9]['value']}
+<br/>${fields[9]['value']}
 <br/>
 <br/><font size='12' face='PuristaMedium'>Air:</font>
-<br/> - ${fields[10]['value']}
+<br/>${fields[10]['value']}
 <br/>
 <br/><font size='12' face='PuristaMedium'>Artillery:</font>
-<br/> - ${fields[11]['value']}
+<br/>${fields[11]['value']}
 <br/>
 <br/><font size='12' face='PuristaMedium'>Explosives:</font>
-<br/> - ${fields[12]['value']}
+<br/>${fields[12]['value']}
 ENDTAB;
 
 NEWTAB("III. Mission:")
 <br/><font color='${sideColour}' size='18' face='PuristaBold'>MISSION INTENT</font>
 <br/>${fields[13]['value']}
-<br/>
-<br/>
-<br/><font color='${sideColour}' size='18' face='PuristaBold'>OBJECTIVES</font>
-<br/><font color='${sideColour}' size='14' face='PuristaSemiBold'>${fields[14]['value']}:</font>
-<br/> - ${fields[15]['value']}
 ENDTAB;
 
 NEWTAB("IV. Support & Logistics:")
 <br/><font color='${sideColour}' size='18' face='PuristaBold'>SUPPORT</font>
 <br/><font color='${sideColour}' size='14' face='PuristaSemiBold'>Available supports:</font>
-<br/> - ${fields[16]['value']}
+<br/>${fields[14]['value']}
 <br/>
 <br/>
 <br/><font color='${sideColour}' size='18' face='PuristaBold'>LOGISTICS</font>
 <br/><font color='${sideColour}' size='14' face='PuristaSemiBold'>Maintenance:</font>
-<br/> - ${fields[17]['value']}
+<br/>${fields[15]['value']}
 <br/>
 <br/><font color='${sideColour}' size='14' face='PuristaSemiBold'>Transportation:</font>
-<br/> - ${fields[18]['value']}
+<br/>${fields[16]['value']}
 <br/>
 <br/><font color='${sideColour}' size='14' face='PuristaSemiBold'>Resupplies:</font>
-<br/> - ${fields[19]['value']}
+<br/>${fields[17]['value']}
 <br/>
 <br/>
 <br/><font color='${sideColour}' size='18' face='PuristaBold'>SIGNALS</font>
 <br/><font color='${sideColour}' size='14' face='PuristaSemiBold'>Radio frequencies</font>
-<br/> - ${fields[20]['value']}
+<br/>${fields[18]['value']}
 <br/>
 ENDTAB;
 
 NEWTAB("V. Notes:")
 <br/><font color='${sideColour}' size='18' face='PuristaBold'>RESPAWNS</font>
 <br/><font color='${sideColour}' size='14' face='PuristaSemiBold'>Player respawn</font>
-<br/> - ${fields[21]['value']}
+<br/>${fields[19]['value']}
 <br/>
 <br/><font color='${sideColour}' size='14' face='PuristaSemiBold'>Asset respawn</font>
-<br/> - ${fields[22]['value']}
+<br/>${fields[20]['value']}
 <br/>
 <br/><font color='${sideColour}' size='14' face='PuristaSemiBold'>Reinsertion</font>
-<br/> - ${fields[23]['value']}
+<br/>${fields[21]['value']}
 <br/>
 <br/>
 <br/><font color='${sideColour}' size='18' face='PuristaBold'>END CONDITIONS</font>
 <br/><font color='${sideColour}' size='14' face='PuristaSemiBold'>Success</font>
-<br/> - ${fields[24]['value']}
+<br/>${fields[22]['value']}
 <br/>
 <br/><font color='${sideColour}' size='14' face='PuristaSemiBold'>Failure</font>
-<br/> - ${fields[25]['value']}
+<br/>${fields[23]['value']}
 <br/>
 <br/>
 <br/><font color='${sideColour}' size='18' face='PuristaBold'>RESTRICTIONS</font>
-<br/> - ${fields[26]['value']}
+<br/>${fields[24]['value']}
 <br/>
 <br/>
 <br/><font color='${sideColour}' size='18' face='PuristaBold'>JIP</font>
-<br/> - ${fields[27]['value']}
+<br/>${fields[25]['value']}
 <br/>
 <br/>
 <br/><font color='${sideColour}' size='18' face='PuristaBold'>Mission maker's notes</font>
-<br/> - ${fields[28]['value']}
+<br/>${fields[26]['value']}
 ENDTAB;`
 
     // Display output
