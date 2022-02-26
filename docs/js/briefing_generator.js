49fb7c4efaaa5c358fcb259ffe195c3ad14ccc75
Dániel Boros <daniel.boros.17@gmail.com>
2022-02-26 16:09:49 +0100

    Briefing - Fix colours in briefing generator (#408)
    * Fix colours in briefing generator
    
    * Remove unnecessary console log

--- docs/js/briefing_generator.js
+++ docs/js/briefing_generator.js
@@ -54,7 +54,7 @@ function generateBriefing(fields) {
     });
 
     // Select side colour
-    let sideColour = fields[27]['value'] === "BLUFOR" ? "#21749c" : "#9c2d21";
+    let sideColour = fields[28]['value'] === "BLUFOR" ? "#21749c" : "#9c2d21";
 
     // Output
     let template = `// This briefing file was generated with the Briefing Builder tool
