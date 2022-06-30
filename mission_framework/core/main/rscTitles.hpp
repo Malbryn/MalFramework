79786d84b439fe32aa969e011a0be194d61a832b
Dániel Boros <daniel.boros.17@gmail.com>
2022-06-30 19:19:51 +0200

    Add Default class to RscTitles

--- mission_framework/core/main/rscTitles.hpp
+++ mission_framework/core/main/rscTitles.hpp
@@ -3,7 +3,14 @@
 #include "..\admin\dialog\MF_AdminMenu.hpp"
 #include "..\end_mission\dialog\MF_EndScreen.hpp"
 
-class rscTitles {
+class RscTitles {
+    class Default {
+        idd = -1;
+        fadein = 0;
+        fadeout = 0;
+        duration = 0;
+    };
+
     #include "..\ao_limit\dialog\RscAOLimit.hpp"
     #include "..\countdown\dialog\RscCountdown.hpp"
     #include "..\setup_timer\dialog\RscSetupTimer.hpp"
